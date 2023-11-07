8997a19bd6a3b7c1141ca591d5edaa63a29c6dad
UN_Known <65482377+unknown-sudo-max@users.noreply.github.com>
2023-11-07 06:45:23 +0200

    Update bkd.php

--- echo/src/bkd.php
+++ echo/src/bkd.php
@@ -1,1 +1,52 @@
 
+<?php
+
+
+// Add a menu item under "Tools" menu
+function search_replace_menu() {
+    add_submenu_page(
+        'tools.php',
+        'Search Replace Plugin',
+        'Search Replace',
+        'manage_options',
+        'search_replace_plugin',
+        'search_replace_page'
+    );
+}
+add_action('admin_menu', 'search_replace_menu');
+
+// Display the search replace page
+function search_replace_page() {
+    if (isset($_POST['submit'])) {
+        $search = sanitize_text_field($_POST['search']);
+        $replace = sanitize_text_field($_POST['replace']);
+
+        if (!empty($search) && !empty($replace)) {
+            // Perform search and replace in the database
+            global $wpdb;
+            $wpdb->query($wpdb->prepare("UPDATE {$wpdb->posts} SET post_content = REPLACE(post_content, %s, %s)", $search, $replace));
+
+            echo '<div class="updated"><p>Search and replace completed.</p></div>';
+        } else {
+            echo '<div class="error"><p>Both search and replace fields are required.</p></div>';
+        }
+    }
+    ?>
+    <div class="wrap">
+        <h2>Search Replace Plugin</h2>
+        <form method="post">
+            <table class="form-table">
+                <tr>
+                    <th scope="row"><label for="search">Search for:</label></th>
+                    <td><input type="text" name="search" id="search" class="regular-text"></td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="replace">Replace with:</label></th>
+                    <td><input type="text" name="replace" id="replace" class="regular-text"></td>
+                </tr>
+            </table>
+            <p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Search Replace"></p>
+        </form>
+    </div>
+    <?php
+}
